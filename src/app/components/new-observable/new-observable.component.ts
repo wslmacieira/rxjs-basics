6e3edcd09bddafabb3b014b2da394d07a6fc9344
wslmacieira <wslmacieira@outlook.com>
2021-11-18 10:42:37 -0300

    Different ways of creating the observer

--- src/app/components/new-observable/new-observable.component.ts
+++ src/app/components/new-observable/new-observable.component.ts
@@ -1,30 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Observer4 } from 'src/app/services/Obsrver4';
 
 @Component({
   selector: 'app-new-observable',
   templateUrl: './new-observable.component.html',
-  styleUrls: ['./new-observable.component.css']
+  styleUrls: ['./new-observable.component.css'],
 })
 export class NewObservableComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     const newObservable = new Observable<number>((observer) => {
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i <= 5; i++) {
+        // if (i === 4) {
+        //   observer.error('unknow error. i value is 4');
+        // }
         observer.next(i);
       }
       observer.complete();
+      observer.next(1000);
     });
 
     let observable = {
-      next: (data: number) => console.log(data),
+      next: (data: number) => console.log('observer 1 ' + data),
       error: (error: string) => console.log(error),
       complete: () => console.log('complete all done'),
     };
 
     newObservable.subscribe(observable);
-  }
 
+    newObservable.subscribe({
+      next: (data: number) => console.log('observer 2 ' + data),
+      error: (error: string) => console.log(error),
+      complete: () => console.log('complete all done'),
+    });
+
+    newObservable.subscribe(
+      (data) => console.log('observer 3 ' + data),
+      (error) => console.log(error),
+      () => console.log('complete done')
+    );
+
+    newObservable.subscribe(new Observer4())
+  }
 }
